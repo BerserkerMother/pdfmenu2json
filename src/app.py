415b123348518bae42d13a361b11d3eb812dc983
Kave Bahraman <sadegh.b218@gmail.com>
2023-10-08 15:52:03 +0330

    Adds support for html websites

--- src/app.py
+++ src/app.py
@@ -5,25 +5,37 @@ from flask import Flask, request
 import dotenv
 
 from logger import logger
-from utils import llm_to_json, get_parsing_chain, get_chunks
+from utils import llm_to_json
+from extractors import pdf, html
 
 # set up logging
 log = logger.get_logger("pdf2menu")
 
-the_chain = get_parsing_chain()
+pdf_chain = pdf.get_parsing_chain()
+html_chain = html.get_parsing_chain()
 
 app = Flask("menu2json")
 
-
-@app.route("/convert", methods=["GET", "POST"])
-def convert():
+@app.route("/pdf", methods=["POST"])
+def pdf_convert():
     pdf_menu = request.files["menu"]
-    log.debug("err  ")
-    chunks = get_chunks(pdf_menu=pdf_menu)
+    chunks = pdf.get_chunks(pdf_menu=pdf_menu)
     menu_items = {"menu": []}
     for chunk in chunks:
-        log.info(chunk)
-        items_unstructured = the_chain.run(chunk)
+        # log.info(chunk)
+        items_unstructured = pdf_chain.run(chunk)
+        llm_to_json(menu_dict=menu_items, items_unstructured=items_unstructured)
+    return menu_items
+
+
+@app.route("/html", methods=["POST"])
+def html_convert():
+    menu_link = request.json["menu"]
+    chunks = html.get_chunks(link=menu_link)
+    menu_items = {"menu": []}
+    for chunk in chunks:
+        # log.info(chunk)
+        items_unstructured = html_chain.run(chunk)
         llm_to_json(menu_dict=menu_items, items_unstructured=items_unstructured)
     return menu_items
 
